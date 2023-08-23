fa2c9c4cb72c3e91230b161c22f6e181c93424aa
Mantas Navickis <navickis@gmail.com>
2023-08-23 18:21:25 +0200

    removed user.js

--- routes/user.js
+++
@@ -1,9 +0,0 @@
-const { getUser, createUser } = require('../domain/user')
-const express = require('express')
-const router = express.Router()
-
-// GET
-router.get('/:id', getUser)
-router.post('/', createUser)
-
-module.exports = router
