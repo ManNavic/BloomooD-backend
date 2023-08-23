28ef49c1be10c53005eac7fd212c3b28b369985f
Mantas Navickis <navickis@gmail.com>
2023-08-23 18:25:35 +0200

    added new routers

--- index.js
+++ index.js
@@ -13,6 +13,19 @@ db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 app.use(express.json())
 app.use(cors())
+
+// Routers
+// User router
+const registerRouter = require('./routes/register')
+app.use('/register', registerRouter)
+// Feed router
+const feedRouter = require('./routes/feed')
+app.use('/feed', feedRouter)
+
+const loginRouters = require('./routes/login')
+app.use('/login', loginRouters)
+const usersRouters = require('./routes/users')
+app.use('/users', tokenAuth, usersRouters)
 const profileRouters = require('./routes/profile')
 app.use('/profile', tokenAuth, profileRouters)
 
