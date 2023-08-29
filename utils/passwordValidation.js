a99e86c23865718d849e540e47e4011ee3841acc
Mantas Navickis <navickis@gmail.com>
2023-08-29 02:08:03 +0200

    added  more detailed error messages

--- utils/passwordValidation.js
+++ utils/passwordValidation.js
@@ -1,8 +1,27 @@
 const passwordValidation = (password) => {
-  const isValid =
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
-      password
+  const hasLowerCase = /[a-z]/.test(password)
+  const hasUpperCase = /[A-Z]/.test(password)
+  const hasDigit = /\d/.test(password)
+  const hasSpecialChar = /[@$!%*?&]/.test(password)
+  const isLengthValid = password.length >= 8
+
+  const errors = []
+  if (!hasLowerCase || !hasUpperCase) {
+    errors.push(
+      'Password must include at least one uppercase letter and lowercase letter.'
     )
-  return isValid
+  }
+  if (!hasDigit) {
+    errors.push('Password must include at least one digit.')
+  }
+  if (!hasSpecialChar) {
+    errors.push('Password must include at least one special character.')
+  }
+  if (!isLengthValid) {
+    errors.push('Password must be at least 8 characters long.')
+  }
+
+  return errors
 }
+
 module.exports = { passwordValidation }
